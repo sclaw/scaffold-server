f46e4dcfa59493199ebeb41f39ece41e9077a820
Sergey Yakovlev <fclaw007@gmail.com>
2019-06-04 21:54:05 +0300

    proto generator haskell

---
+++ aux/generate-proto-haskell.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -eu
+
+sources_hs () { find "${D}" -name '*.hs'; }
+sources_proto () { find proto -name '*.proto'; }
+
+D="${1:-backend/proto}"
+mkdir -p "${D}"
+
+P='1'
+
+# cities_proto | xargs python aux/cities "aux/cities.xlsx"
+sources_hs | xargs truncate -c -s0 /tmp/placeholder
+sources_proto | xargs stack exec hprotoc -- -I proto -d "${D}" --lenses
+find "${D}" -empty -delete
+sources_hs | xargs sed -i -r -e '/OPTIONS_GHC/c\{-# OPTIONS_GHC -w #-}'
+#sources_hs | xargs touch -r .git/modules/proto/HEAD
