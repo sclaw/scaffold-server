6fac5ac0cc763f3d11c520e5967fca4ce6a13915
Sergey Yakovlev <fclaw007@gmail.com>
2019-08-01 16:47:33 +0300

    js proto

--- frontend/generate-proto-js.sh
+++ frontend/generate-proto-js.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-grpc_tools_node_protoc --proto_path=../proto --js_out=import_style=commonjs,binary:proto  ../proto/*
-
+grpc_tools_node_protoc --proto_path=../proto  --js_out=import_style=commonjs,binary:proto \
+   ../proto/Time/* \
+   ../proto/Protobuf/* \
+   ../proto/EdgeNode/User.proto \
+   ../proto/EdgeNode/Rbac.proto \
+   ../proto/EdgeNode/Error.proto \
+   ../proto/EdgeNode/Api/Http/Auth/* \
+   ../proto/EdgeNode/Service/* \
+   ../proto/EdgeNode/User/*
