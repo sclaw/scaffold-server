7033739f9fa8cc690b201bcc838f5ef2aaea8296
Sergey Yakovlev <fclaw007@gmail.com>
2019-08-30 12:13:03 +0300

    new version of hash creating

--- src/core/Hash.hs
+++ src/core/Hash.hs
@@ -1,9 +1,9 @@
 module Hash (mkHash) where
 
-import Data.ByteArray.Encoding (convertToBase, Base (Base64))
-import Data.ByteString (ByteString)
 import Control.Lens.Iso.Extended    
 import Control.Lens
+import Crypto.Hash
+import Data.ByteString
 
 mkHash :: Show a => a -> ByteString
-mkHash x = convertToBase Base64 (show x^.stext.textbs)
+mkHash x = show (hash (show x^.stext.textbs) :: SHA256)^.stext.textbs
