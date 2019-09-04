b213f93dcd5cc4274d86cecac0160e6c0ce93a37
Sergey Yakovlev <fclaw007@gmail.com>
2019-09-04 13:50:09 +0300

    migration fix

--- src/core/Database/Migration/Version/Version15.hs
+++ src/core/Database/Migration/Version/Version15.hs
@@ -11,7 +11,7 @@ sql = [i|alter table "edgeNode"."QualificationProviderFeatures"
          rename column "qualififcationKey" to "qualificationKey";
          insert into "edgeNode"."QualificationProviderFeatures"
          ("qualificationKey", "academicAreas", "durationPeriod",
-          "tuitionFees", "admissionDeadline", "language", "studyMode")
+          "tuitionFeesPerAnnum", "admissionDeadline", "language", "studyMode")
          values (7, '["Engineering", "Computing"]', 1, null, date '2019-06-14',
          '#{fromLanguage LanguageEnglish}', 'Full-time');
       |]
