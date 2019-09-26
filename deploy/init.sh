8cee6ff5d338f1966fa163f52cd4ec634b301771
Sergey Yakovlev <fclaw007@gmail.com>
2019-09-26 18:53:12 +0300

    deploy

--- deploy/init.sh
+++ deploy/init.sh
@@ -5,4 +5,4 @@ cd /home/nix/
 
 sleep 2
 echo 'launch server..'
-./bin/edge-node-server --cfgPath config.yaml +RTS -N -T -s -A64m
+./bin/edge-node-server --cfgPath deploy/config.yaml +RTS -N -T -s -A64m
