0fc71c6b63d96211b47c2eeb0e1dcf90d70db1b5
Sergey Yakovlev <fclaw007@gmail.com>
2020-03-07 23:07:42 +0300

    1

--- shell.nix
+++ shell.nix
@@ -1,9 +1,8 @@
 {ghc}:
 with (import <nixpkgs> {});
 
-
 haskell.lib.buildStackProject {
   inherit ghc;
   name = "server";
-  buildInputs = [postgresql lzma.dev git zlib perl];
+  buildInputs = [ postgresql lzma.dev git zlib perl];
 }
