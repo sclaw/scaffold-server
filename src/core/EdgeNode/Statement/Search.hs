3700e6a9a5fb2c89933311f730cc62b87b294c4f
Sergey Yakovlev <fclaw007@gmail.com>
2020-03-28 14:09:59 +0300

    search

--- src/core/EdgeNode/Statement/Search.hs
+++ src/core/EdgeNode/Statement/Search.hs
@@ -17,7 +17,16 @@ getQualification = statement $ premap mkItem list
   where
     statement = 
       [foldStatement|
+        with get_qualification_matched_ids as
+         (select x.ident
+          from (
+           select 
+            distinct provider_branch_qualification_fk as ident, 
+            similarity($1 :: text, piece) as match_per 
+           from search.qualification) as x
+          where (x.match_per > 0)
+          order by x.match_per desc) 
         select id :: int8, title :: text
         from edgenode.provider_branch_qualification 
-        where id = any(select id from search.qualification where $1 :: text % piece)|]
+        where id = any(select ident from get_qualification_matched_ids)|]
     mkItem x = SearchQualification (x^._1.integral) (x^._2.from lazytext)
