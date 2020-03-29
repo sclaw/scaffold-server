410fac2372015ee8aad434ae017e5af0c24c217f
Sergey Yakovlev <fclaw007@gmail.com>
2020-03-29 13:29:15 +0300

    search fix

--- src/core/EdgeNode/Statement/Search.hs
+++ src/core/EdgeNode/Statement/Search.hs
@@ -22,36 +22,28 @@ getBarItems = statement $ (fmap SearchBar . premap (^.from lazytext)) vector
       [foldStatement|
         with 
           get_qualification_matched_ids as 
-           (select 
-             provider_branch_qualification_fk as ident, 
-             origin, 
+           (select
+             piece, 
              word_similarity($1 :: text, piece) as sim
             from search.qualification
             where word_similarity($1 :: text, piece) >= 0.5
-            order by ident
             limit 10),
           get_provider_matched_ids as 
            (select 
-             provider_branch_fk as ident, 
-             origin, 
+             piece, 
              word_similarity($1 :: text, piece) as sim
             from search.provider
             where word_similarity($1 :: text, piece) >= 0.5
-            order by ident
             limit 10),
           get_combined_search as 
-            (select pbq.title, sq.sim
-             from get_qualification_matched_ids as sq 
-             inner join edgenode.provider_branch_qualification as pbq
-             on sq.ident = pbq.id
+            (select piece, sim
+             from get_qualification_matched_ids
              union 
-             select pb.title, sb.sim
-             from get_provider_matched_ids as sb 
-             inner join edgenode.provider_branch as pb
-             on sb.ident = pb.id
+             select piece, sim
+             from get_provider_matched_ids
              order by sim desc
              limit 10)
-        select title :: text from get_combined_search|]
+        select piece :: text from get_combined_search|]
 
 getQualificationList :: HS.Statement T.Text SearchQualificationList
 getQualificationList = undefined
