4e087705d761b51c49ba049e37355ba390cbf242
Sergey Yakovlev <fclaw007@gmail.com>
2020-04-25 08:35:51 +0300

    enum

--- src/core/EdgeNode/Controller/Service/Enum/GetCountries.hs
+++ src/core/EdgeNode/Controller/Service/Enum/GetCountries.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module EdgeNode.Controller.Service.Enum.GetCountries (controller) where
 
@@ -8,6 +9,7 @@ import qualified  EdgeNode.Transport.Error as Error
 import EdgeNode.Country
 
 import TH.Proto
+import Katip
 import KatipController
 import qualified Data.Text as T
 import Data.Aeson.WithField
@@ -16,14 +18,21 @@ import Control.Monad.IO.Class
 import Data.Yaml
 import System.FilePath.Posix
 import Control.Lens
-import Control.Lens.Iso.Extended
 import Data.Maybe
+import Data.Bitraversable
+import Data.Traversable
 
 controller :: EdgeNodeLanguage -> KatipController (Response [WithField "enum" Country T.Text])
-controller country = liftIO $ do
-  dir <- getCurrentDirectory
+controller country = do
+  dir <- liftIO getCurrentDirectory
   let country_dir = dir </> "enum" </> "country"
   let path = country_dir </> country^.isoEdgeNodeLanguage <> ".yaml"
-  let mkError e = Error.asError $ prettyPrintParseException e^.stext
-  let mkDef (WithField x val_m) = WithField x $ fromMaybe (show x^.stext) val_m
-  fmap (fromEither . bimap mkError (map mkDef)) $ decodeFileEither @[(WithField "enum" Country (Maybe T.Text))] path
+  let error e = do
+        $(logTM) ErrorS (logStr (prettyPrintParseException e))
+        pure $ Error.asError @T.Text "error while getting country"
+  let ok xs = do
+        $(logTM) DebugS (logStr (show xs))
+        for xs $ \(WithField x val_m) ->
+          pure $ fmap (WithField x) val_m
+  resp <- liftIO $ decodeFileEither @[(WithField "enum" Country (Maybe T.Text))] path
+  fmap (fmap catMaybes . fromEither) $ bitraverse error ok resp
