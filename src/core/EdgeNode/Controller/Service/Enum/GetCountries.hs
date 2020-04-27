3a0f438ef1e8483db2bb582334a7288c3061dba9
Sergey Yakovlev <fclaw007@gmail.com>
2020-04-27 21:26:07 +0300

    fix enum

--- src/core/EdgeNode/Controller/Service/Enum/GetCountries.hs
+++ src/core/EdgeNode/Controller/Service/Enum/GetCountries.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module EdgeNode.Controller.Service.Enum.GetCountries (controller) where
 
@@ -22,15 +23,23 @@ import Control.Lens
 import Data.Maybe
 import Data.Bitraversable
 import Data.Traversable
+import BuildInfo
+import Pretty
+import Control.Concurrent.Lifted
+import Control.Monad
+import Control.Lens.Iso.Extended
 
 controller :: EdgeNodeLanguage -> KatipController (Response [WithField "enum" Country T.Text])
 controller lang = do
   dir <- liftIO getCurrentDirectory
-  let country_dir = dir </> "enum" </> "lang"
+  let country_dir = dir </> "enum" </> "country"
   let path = country_dir </> lang^.isoEdgeNodeLanguage <> ".yaml"
   let error e = do
         $(logTM) ErrorS (logStr (prettyPrintParseException e))
-        pure $ Error.asError @T.Text "error while getting lang"
+        telegram_service <- fmap (^.katipEnv.telegram) ask
+        logger <- askLoggerIO
+        void $ fork $ liftIO $ send telegram_service logger (mkPretty $location (prettyPrintParseException e)^.stext)
+        pure $ Error.asError @T.Text "error while getting country"
   let ok xs = do
         $(logTM) DebugS (logStr (show xs))
         for xs $ \(WithField x val_m) ->
