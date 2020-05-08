2b91424e699e0ae1df6961822e09ef8d204445f8
Sergey Yakovlev <fclaw007@gmail.com>
2020-05-08 17:13:47 +0300

    stack

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
-with (import <nixpkgs> {});
-haskell.lib.buildStackProject {
+{ pkgs ? import <nixpkgs> {}, ghc ? pkgs.ghc }:
+pkgs.haskell.lib.buildStackProject {
   inherit ghc;
   name = "server";
-  buildInputs = [postgresql lzma.dev git zlib imagemagick];
+  buildInputs = with pkgs; [postgresql lzma.dev git zlib imagemagick];
 }
