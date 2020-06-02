6ba26db8734332ccbf60665ae50d2b205bc40064
Sergey Yakovlev <fclaw007@gmail.com>
2020-06-02 16:32:11 +0300

    user suggestion

--- src/core/EdgeNode/Controller/User/Qualification/MarkSuggestion.hs
+++ src/core/EdgeNode/Controller/User/Qualification/MarkSuggestion.hs
@@ -23,8 +23,10 @@ import Data.Coerce
 import Proto3.Suite.Types
 import Data.Int
 import qualified Data.Text as T
-import Data.Foldable
+import Data.Traversable
 import Data.Aeson.WithField
+import Data.Maybe
+import Data.Functor
 
 controller :: Id "suggestion" -> QualificationSuggestion -> UserId -> KatipController (Response Unit)
 controller _ suggestion _ |
@@ -33,8 +35,12 @@ controller _ suggestion _ |
   Right TrajectoryStatusNew = pure $ Error $ asError @T.Text "wrong status"
 controller suggestion_id suggestion user_id = do
   runTelegram $location (suggestion_id, suggestion, user_id)
-  $(logTM) DebugS $ logStr $ "mark suggestion: " <> show (suggestion_id, suggestion, user_id)
+  $(logTM) DebugS $ logStr $
+    "mark suggestion: " <>
+    show (suggestion_id, suggestion, user_id)
   hasql <- fmap (^.katipEnv.hasqlDbPool) ask
-  qual_m <- katipTransaction hasql $ statement User.markSuggestion (suggestion_id, user_id, suggestion)
-  for_ qual_m $ \ident -> Add.controller (coerce ident) user_id
-  pure $ Ok Unit
+  qual_m <- katipTransaction hasql $
+    statement User.markSuggestion (suggestion_id, user_id, suggestion)
+  fmap (fromMaybe (Ok Unit)) $ for qual_m $ \ident -> do
+    add_response <- Add.controller (coerce ident) user_id
+    runTelegram $location add_response $> add_response
